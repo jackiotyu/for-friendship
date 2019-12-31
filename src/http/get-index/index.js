29e56ea9d1a6d34c4b556fdf894ddee30af56a5c
jackiotyu <2504448153@qq.com>
2019-12-31 17:29:48 +0800

    修正图片比例

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,40 +1,42 @@
-const Main = require('@architect/views/main.js')
-const staticAssetsHelper = require('./static-assets-helper')
+const Main = require("@architect/views/main.js");
+const staticAssetsHelper = require("./static-assets-helper");
 
 // Customize your site by changing the data below
-exports.handler = async function Index () {
+exports.handler = async function Index() {
   let body = Main({
     /**
      * Basic bio
      */
-     // location: 'West Glacier, MT',
-    fullname: '荣耀战魂小站', // ←  Start by adding your name!
-    title: '荣耀战魂小站',
-    occupation: 'Artist & Photographer',
-    bio: '敬请期待',
+    // location: 'West Glacier, MT',
+    fullname: "荣耀战魂小站", // ←  Start by adding your name!
+    title: "荣耀战魂小站",
+    occupation: "Artist & Photographer",
+    bio: "敬请期待",
 
     /**
      * Contact / social
      * - Comment out any item below to remove it from your page
      */
-    email: '2504448153@email.com',
+    email: "2504448153@email.com",
 
     /**
      * Layout
      */
-    photographer: '育碧成都',
-    service: 'Ubisoft',
-    credit: 'https://bird-fnd-staging.begin.app/',
+    photographer: "育碧成都",
+    service: "Ubisoft",
+    credit: "https://bird-fnd-staging.begin.app/",
     //image: staticAssetsHelper('background.jpg')
     // or link to an external image URL such as ↓
-    image: 'https://cdn.jsdelivr.net/gh/jackiotyu/ImgCloud/master/wulin/Zhanhu/armor/common%20s12/Xiaowei.jpg'
-  })
+    image:
+      "https://cdn.jsdelivr.net/gh/jackiotyu/ImgCloud/blob/master/wulin/Zhanhu/armor/common%20s12/Xiaowei.jpg"
+  });
 
   return {
     headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      "content-type": "text/html; charset=utf8",
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0"
     },
     body
-  }
-}
+  };
+};
